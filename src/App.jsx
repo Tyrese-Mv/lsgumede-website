f18638873e89e7b4730c90687b431669fcce2722
Tyrese-Mv <musawenkosi106@gmail.com>
2025-10-13 14:26:49 +0200

    Add SplashScreen and ScrollToTop components to App layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,23 @@ import Home from './pages/Home'
 import About from './pages/About'
 import ServicesPage from './pages/ServicesPage'
 import ContactPage from './pages/ContactPage'
+import ScrollToTop from './components/ScrollToTop'
+import SplashScreen from './components/SplashScreen'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="services" element={<ServicesPage />} />
-        <Route path="contact" element={<ContactPage />} />
-      </Route>
-    </Routes>
+    <>
+      <SplashScreen />
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="services" element={<ServicesPage />} />
+          <Route path="contact" element={<ContactPage />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
 
